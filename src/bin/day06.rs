985e0d1fb65b561e875e93e741cf4663fd02692f
Alexander Ganslandt <alexanga@axis.com>
2025-12-14 20:07:35 +0100

    Day 06: Part 2
    Change-Id: I6038152d7bbbde2f46ba51babe9b60b484c702ec

--- src/bin/day06.rs
+++ src/bin/day06.rs
@@ -3,15 +3,11 @@ use std::fs;
 fn compute_numbers(digits: &Vec<Vec<char>>, operation: char, mut cols: (u32, u32)) -> u64 {
     let mut numbers = Vec::new();
 
-    for row in digits {
+    for col in (cols.0..cols.1).rev() {
         let mut number = String::new();
 
-        if cols.1 == 0 {
-            cols.1 = row.len() as u32
-        };
-
-        for i in cols.0..cols.1 {
-            number.push(row[i as usize]);
+        for row in digits {
+            number.push(*row.get(col as usize).unwrap_or(&' '));
         }
 
         numbers.push(number.trim().parse::<u64>().unwrap());
@@ -29,12 +25,23 @@ fn main() {
         fs::read_to_string("inputs/day06/input.txt").expect("Failed to read example.txt");
 
     let mut digits = Vec::new();
+    let mut max_len = 0;
     for line in contents.lines() {
         let d: Vec<char> = line.chars().collect();
+
+        max_len = max_len.max(d.len());
         digits.push(d);
     }
 
-    let operations = digits.pop().unwrap();
+    /* Pad rows */
+    for row in &mut digits {
+        while row.len() <= max_len {
+            row.push(' ');
+        }
+    }
+
+    let mut operations = digits.pop().unwrap();
+    operations.push('s');
 
     for d in &digits {
         println!("{:?}", d);
@@ -43,22 +50,23 @@ fn main() {
 
     let mut i = 0;
     let mut total = 0;
-    while i < operations.len() {
+    let mut breaker = false;
+    loop {
         let op = operations[i];
 
         println!("op: {}", op);
 
         let mut cols = (i as u32, 0);
 
-        if i + 1 >= operations.len() {
-            cols.1 = 0;
-        } else {
-            for n in i + 1..operations.len() {
-                if operations[n] != ' ' {
-                    cols.1 = n as u32;
-                    i = n;
-                    break;
+        for n in i + 1..operations.len() {
+            if operations[n] != ' ' {
+                cols.1 = (n - 1) as u32;
+                i = n;
+
+                if operations[n] == 's' {
+                    breaker = true;
                 }
+                break;
             }
         }
 
@@ -66,7 +74,7 @@ fn main() {
         println!("Result: {}", ret);
         total += ret;
 
-        if cols.1 == 0 {
+        if breaker {
             break;
         }
     }
