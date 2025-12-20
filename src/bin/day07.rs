5936a7f1a80499e3f5fb90edb20f19215b11cf4f
Alexander Ganslandt <alexanga@axis.com>
2025-12-20 15:39:13 +0100

    Day 07: Part 2
    Change-Id: I212c6e6abf7b6695c9d4ece2bf1101a5361e57e4

--- src/bin/day07.rs
+++ src/bin/day07.rs
@@ -1,12 +1,12 @@
-use std::fs;
 use aoc2025::map;
+use std::{collections::HashMap, fs};
 
 fn main() {
     let contents =
         fs::read_to_string("inputs/day07/input.txt").expect("Failed to read example.txt");
 
     let mut map = map::Map::from_string(&contents);
-    let mut splits = 0;
+    let mut splits: HashMap<(usize, usize), u64> = HashMap::new();
 
     for y in 0..map.grid.len() {
         for x in 0..map.grid[y].len() {
@@ -15,20 +15,29 @@ fn main() {
             if y == 0 {
                 if e == 'S' {
                     map.set(x, y, '|');
+                    *splits.entry((x, y)).or_insert(0) += 1;
                 }
                 continue;
             }
 
             let e_up = *map.get(x, y - 1).unwrap();
             if e_up == '|' {
+                let timelines = *splits.entry((x, y - 1)).or_insert(0);
+
                 match e {
                     '.' => {
                         map.set(x, y, '|');
+                        *splits.entry((x, y)).or_insert(0) += timelines;
                     }
                     '^' => {
                         map.set(x - 1, y, '|');
                         map.set(x + 1, y, '|');
-                        splits += 1;
+
+                        *splits.entry((x - 1, y)).or_insert(0) += timelines;
+                        *splits.entry((x + 1, y)).or_insert(0) += timelines;
+                    }
+                    '|' => {
+                        *splits.entry((x, y)).or_insert(0) += timelines;
                     }
                     _ => {}
                 }
@@ -36,6 +45,10 @@ fn main() {
         }
     }
 
-    map.print();
-    println!("{}", splits);
+    let sum: u64 = splits
+        .iter()
+        .filter(|(&(_, y), &_)| y == map.grid.len() - 1)
+        .map(|(_, v)| v)
+        .sum();
+    println!("{}", sum);
 }
